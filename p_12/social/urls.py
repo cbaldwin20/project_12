995630e134159de8183512bcc84b8a0bb374f478
cbaldwin <cbaldwin20@yandex.com>
2018-05-22 13:35:36 -0600

    I fixed the logout situation, but I'm going now make a profile_new in the views and split up the def profile_edit.

--- p_12/social/urls.py
+++ p_12/social/urls.py
@@ -9,7 +9,7 @@ app_name = 'social'
 
 urlpatterns = [
 	url(r'^$', views.index, name='home'),
-    url(r'^(?P<need>[-\w]+)/$', views.index, name='home_need'),
+    url(r'^(?P<need>[-\w]+)$', views.index, name='home_need'),
     url(r'^search$', views.search, name='search'),
     url(r'^project_new$', views.project_new, name='project_new'),
     url(r'^project_edit/(?P<url_slug>[-\w]+)/$', views.project_edit, name='project_edit'),
@@ -18,7 +18,7 @@ urlpatterns = [
 
     url(r'^profile_edit$', views.profile_edit, name='profile_edit'),
     url(r'^profile/(?P<url_slug>[-\w]+)/$', views.profile, name='profile'),
-    url(r'^applications/$', views.applications, name='applications'),
+    url(r'^applications/$', views.applications, name='start_applications'),
     url(r'^applications/(?P<applications>[-\w]+)/(?P<project>[-\w]+)/(?P<need>[-\w]+)/$', views.applications, name='applications'),
     url(r'^search/(?P<need>[-\w]+)/$', views.search, name='search'),
 
